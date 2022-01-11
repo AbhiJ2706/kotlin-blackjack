15f4143346f722f384569b258730c8939330cc21
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2022-01-10 21:53:27 -0500

    initial commit

---
+++ blackjack.kt
@@ -0,0 +1,108 @@
+enum class Suits {
+    Spades, Hearts, Diamonds, Clubs
+}
+
+data class Card(val suit: String, val worth: Int, val tag: String) {
+    override fun equals(other: Any?) : Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as Card
+
+        return tag == other.tag
+    }
+}
+
+data class Deck(val temp: Int) {
+    val tagsToVals: Map<String, out Int> = mapOf(
+        "A" to 11,
+        "2" to 2,
+        "3" to 3,
+        "4" to 4,
+        "5" to 5,
+        "6" to 6,
+        "7" to 7,
+        "8" to 8,
+        "9" to 9,
+        "T" to 10,
+        "J" to 10,
+        "Q" to 10,
+        "K" to 10
+    )
+    var cards = ArrayList<Card>()
+    init {
+        build()
+    }
+    fun build() {
+        for (suit in Suits.values()) {
+            for ((k, v) in tagsToVals) {
+                var s: String = when(suit) {
+                    Suits.Spades -> "S"
+                    Suits.Hearts -> "H"
+                    Suits.Diamonds -> "D"
+                    Suits.Clubs -> "C"
+                    else -> "N"
+                }
+                cards.add(Card(s, v, k))
+            }
+        }
+    }
+}
+
+class Hand() {
+    var cards = ArrayList<Card>()
+    var size = 0
+    constructor(c: Card) : this() {
+        cards.add(c)
+        size++
+    }
+    fun receiveCard(c: Card) {
+        cards.add(c)
+        size++
+    }
+    operator fun get(i: Int): Card {
+        return cards[i]
+    }
+}
+
+open class Human {
+    var tag: String
+    var hands: ArrayList<Hand>
+    var money: Int
+
+    constructor()  
+
+    constructor(name: String) : this() {
+        this.tag = name
+    }
+    constructor(name: String, amt: Int) : this(name) {
+        this.money = amt
+    }
+    init {
+        hands.add(Hand())
+    }
+    fun receiveCard(c: Card) {
+        for (h in hands) {
+            if (h.size == 1 && h[0] == c) {
+                hands.add(Hand(c))
+            }
+        }
+    }
+}
+
+class Player(name: String): Human(name) {
+    constructor(name: String, amt: Int) : super(name, amt)
+}
+
+class Dealer(name: String): Human(name) {
+    constructor(name: String, amt: Int) : super(name, amt)
+}
+
+fun main() {
+    var h = Human("Abhi", 200)
+    println("${h.money}")
+    var c = Card("S", 10, "T")
+    println("${c.suit}, ${c.worth}")
+    var d = Card("S", 10, "T")
+    println("${c == d}")
+}
